95c376ae0d99b8cd204c85b8620aa667b6904f3a
João Luz <joaoluz900@gmail.com>
2021-01-14 16:37:53 +0000

    .
    remoção de pastas excessivas

--- EscalaRepository.java
+++
@@ -1,9 +0,0 @@
-package pt.iade.footprint4all.models.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import pt.iade.footprint4all.models.Escala;
-
-public interface EscalaRepository extends CrudRepository<Escala, Integer> {
-
-}
-    
--- PerTipoRepository.java
+++
@@ -1,8 +0,0 @@
-package pt.iade.footprint4all.models.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import pt.iade.footprint4all.models.PerTipo;
-
-public interface PerTipoRepository extends CrudRepository<PerTipo, Integer> {
-    
-}
